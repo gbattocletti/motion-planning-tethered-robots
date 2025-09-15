e4279a3fbda4ca7ba4aab6203a8a084d88ea3da7
gbattocletti <g.battocletti@tudelft.nl>
2025-09-15 22:54:08 +0200

    Replace script with test

---
+++ tests/test_triangulation.py
@@ -0,0 +1,103 @@
+import os
+
+import matplotlib.pyplot as plt
+import pytest
+
+from tethered_planning.env import env_2d
+from tethered_planning.env.triangulation import Triangulation
+from tethered_planning.utils import io, plot
+from tethered_planning.utils.settings import Settings
+
+
+@pytest.fixture(scope="module", name="plot_settings")
+def fixture_plot_settings():
+    SHOW_PLOT = True
+    BLOCKING = True
+    WAIT_TIME = 1
+    SAVE_PLOT = False
+    return SHOW_PLOT, BLOCKING, WAIT_TIME, SAVE_PLOT
+
+
+@pytest.fixture(name="settings")
+def fixture_settings(env_name):
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))  # Move to script directory
+    settings = Settings()
+    settings.env_name = env_name
+    return settings
+
+
+@pytest.fixture(name="env")
+def fixture_env(settings):
+    env = env_2d.Env2D(settings)
+    return env
+
+
+def show_plot(SHOW_PLOT, BLOCKING, WAIT_TIME):
+    if SHOW_PLOT:
+        if not BLOCKING:
+            plt.show(block=BLOCKING)
+            plt.pause(WAIT_TIME)
+            plt.close()
+        else:
+            plt.show()  # wait on user to close plot and continue
+
+
+@pytest.mark.parametrize(
+    "env_name",
+    [
+        "test_env_1.yaml",
+        "test_env_2.yaml",
+        "test_env_3.yaml",
+        "test_env_4.yaml",
+    ],
+)
+def test_triangulation(settings, env, plot_settings):
+
+    # Unpack fixtures
+    SHOW_PLOT, BLOCKING, WAIT_TIME, SAVE_PLOT = plot_settings
+
+    # Create triangulation
+    triang = Triangulation(env)
+    triang.triangulate()
+
+    # Generate figure
+    fig, _ = plot.plot_graph(
+        triang.vertices,
+        triang.edges,
+        env,
+        settings,
+        nodes_dual=triang.vertices_dual,
+        edges_dual=triang.edges_dual,
+    )
+
+    # Show and/or save figure
+    show_plot(SHOW_PLOT, BLOCKING, WAIT_TIME)
+    if SAVE_PLOT:
+        fig_name = "env"
+        io.save_figure(fig, settings, fig_name, "png")
+
+
+@pytest.mark.parametrize(
+    "env_name",
+    [
+        "test_env_1.yaml",
+    ],
+)
+def test_lift_triangulation(settings, env, plot_settings):
+
+    # Unpack fixtures
+    SHOW_PLOT, BLOCKING, WAIT_TIME, SAVE_PLOT = plot_settings
+
+    # Create triangulation and lift it
+    triang = Triangulation(env)
+    triang.triangulate()
+    triang.lift_triangulation()
+
+    # Generate plot of lifted triangulation
+    fig, _ = plot.plot_lifted_triangulation(triang, env, settings)
+
+    # Show and/or save figure
+    show_plot(SHOW_PLOT, BLOCKING, WAIT_TIME)
+    if SAVE_PLOT:
+        fig_name = "lifted_triangulation"
+        io.save_figure(fig, settings, fig_name, "png")
