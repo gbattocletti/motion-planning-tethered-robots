c332ff13c8ce730f8abf462ed457fbee2a1dffc4
gbattocletti <g.battocletti@tudelft.nl>
2025-11-04 14:06:43 +0100

    Finalize comparison experiments

--- scripts/graph_comparison.py
+++ scripts/graph_comparison.py
@@ -9,17 +9,13 @@ import timeit
 from datetime import datetime
 from pickletools import optimize
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 from tethered_planning.env import env_2d
 from tethered_planning.env.grid_graph import GridGraph
 from tethered_planning.env.triangulation import Triangulation
-from tethered_planning.utils import plot, plot_graph, plot_triangulation
+from tethered_planning.utils.colors import CmdColors
 from tethered_planning.utils.settings import Settings
-
-# Script settings
-SHOW_PLOTS = False
 
 # Evaluation cases
 # Each case is defined as a tuple (env_filename, obstacles m, tether length l), where:
@@ -29,15 +25,19 @@ SHOW_PLOTS = False
 # - l: float, tether length
 eval_cases = [
     ("env_1.yaml", 1, 10.0),
+    ("env_1.yaml", 1, 12.0),
     ("env_1.yaml", 1, 15.0),
     ("env_1.yaml", 1, 20.0),
     ("env_2.yaml", 2, 10.0),
+    ("env_2.yaml", 2, 12.0),
     ("env_2.yaml", 2, 15.0),
     ("env_2.yaml", 2, 20.0),
     ("env_3.yaml", 6, 10.0),
+    ("env_3.yaml", 6, 12.0),
     ("env_3.yaml", 6, 15.0),
     ("env_3.yaml", 6, 20.0),
     ("env_4.yaml", 8, 10.0),
+    ("env_4.yaml", 8, 12.0),
     ("env_4.yaml", 8, 15.0),
     ("env_4.yaml", 8, 20.0),
 ]
@@ -49,68 +49,50 @@ os.chdir(dir_name)
 
 # Initialize settings
 settings = Settings(create_sim_folder=False)
-n_runs = 1  # number of runs for time averaging
+n_runs = 5  # number of runs for time averaging
 
 # Initialize results table
 # Results table columns:
-#   - text index (int)
+#   - env name (str)
 #   - num obstacles m (int)
 #   - tether length (float)
 #   - num of triangles in base triangulation (int)
 #   - num of triangles in simplicial complex (nodes in dual lifted graph) (int)
-#   - num of nodes in simplicial complex (primal lifted graph) (int)
-#   - num of homotopy classes in simplicial complex (int)
 #   - computation time for simplicial complex (float, seconds)
 #   - num of nodes in homotopy augmented graph (int)
-#   - num of homotopy classes in homotopy augmented graph (int)
 #   - computation time for homotopy augmented graph (float, seconds)
-results_table = np.zeros((len(eval_cases), 11), dtype=float)
-
-# Init helper variables
-env_name_old = ""
-env = None
+# The last two columns are repeated for multiple grid resolutions (1, 0.5, 0.1)
+results_table = np.zeros((len(eval_cases), 12), dtype=float)
 
 # Loop over evaluation cases
 for idx, case in enumerate(eval_cases):
     print(
-        f"Running evaluation {idx+1} [{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]"
+        f"\n[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Running "
+        f"evaluation {idx+1} (env {case[0][4]} l={case[2]})"
     )
 
     # Unpack case
     env_name, m, l = case
-    results_table[idx, 0] = idx + 1  # text index
+    results_table[idx, 0] = int(env_name[4])  # env name
     results_table[idx, 1] = m  # num obstacles
     results_table[idx, 2] = l  # tether length
 
     # Generate env for the test case
-    if env_name_old != env_name:
-        settings.env_name = env_name  # change environment
-        env = env_2d.Env2D(settings)
-        env_name_old = env_name
-
-        # Show env plot if enabled
-        if SHOW_PLOTS is True:
-            plot.plot_env(
-                env,
-                show_goal=False,
-                show_anchor=True,
-                show_robot=False,
-                show_tether=False,
-                show_generators=True,
-                show_generators_labels=True,
-            )
-
+    settings.env_name = env_name  # change environment
+    env = env_2d.Env2D(settings)
     # Create triangulation
     triang = Triangulation(env)
-    triang.DEBUG = True  # Enable debug info
+    triang.INFO = True  # Enable info
+    triang.DEBUG = False  # Disable verbose debug prints
     triang.triangulate()
     results_table[idx, 3] = triang.triangles.shape[0]
 
     # Define settings for lifted simplicial complex
-    triang.max_lifted_triangles = 10_000
+    triang.max_lifted_triangles = 100_000
     triang.set_max_dist(l)  # max tether length
 
     # Create lifted simplicial complex
+    print(f"{CmdColors.WARNING}[Triang]{CmdColors.ENDC} Running triangulation.")
     t = (
         timeit.timeit(
             lambda triang=triang: triang.lift_triangulation(),
@@ -118,28 +100,37 @@ for idx, case in enumerate(eval_cases):
         )
         / n_runs
     )
-
-    # Show plots if enabled
-    if SHOW_PLOTS is True:
-        plot_triangulation.plot_3d(triang, env, multi_layer_triangles=True)
-
-    # Add results to table
-    results_table[idx, 4] = len(
-        triang.triangles_lift
-    )  # triangles in simplicial complex
-    results_table[idx, 5] = len(
-        triang.vertices_lift
-    )  # nodes simplicial complex (vertices of triangles, or primal lifted graph)
-    results_table[idx, 6] = len(
-        plot_triangulation.get_unique_signatures(triang)
-    )  # num homotopy classes in simplicial complex
-    results_table[idx, 7] = t  # time simplicial complex
+    results_table[idx, 4] = len(triang.triangles_lift)  # triangs in simplicial complex
+    results_table[idx, 5] = t  # time simplicial complex
 
     # Create homotopy augmented graph
     graph = GridGraph(env)
-    graph.DEBUG = True  # Enable debug info
+    graph.INFO = True  # Enable info
+    graph.DEBUG = False  # Disable verbose debug prints
     graph.set_max_dist(l)
-    graph.n_max = 200_000  # increase max number of nodes for this test
+    graph.n_max = 1_000_000  # increase max number of nodes for this test
+
+    # Coarse resolution
+    print(
+        f"{CmdColors.WARNING}[GridGraph]{CmdColors.ENDC} Running graph with "
+        "resolution 1."
+    )
+    graph.set_grid_resolution(1, 1)
+    t = (
+        timeit.timeit(
+            lambda graph=graph: graph.build_homotopy_augmented_graph(),
+            number=n_runs,
+        )
+        / n_runs
+    )
+    results_table[idx, 6] = len(graph.vertices_lift)  # nodes homotopy augmented graph
+    results_table[idx, 7] = t  # time homotopy augmented graph
+
+    # Medium resolution
+    print(
+        f"{CmdColors.WARNING}[GridGraph]{CmdColors.ENDC} Running graph with "
+        "resolution 0.5."
+    )
     graph.set_grid_resolution(0.5, 0.5)
     t = (
         timeit.timeit(
@@ -148,17 +139,25 @@ for idx, case in enumerate(eval_cases):
         )
         / n_runs
     )
-
-    # Show plots if enabled
-    if SHOW_PLOTS is True:
-        plot_graph.plot_3d(graph, env)
-
-    # Add results to table
     results_table[idx, 8] = len(graph.vertices_lift)  # nodes homotopy augmented graph
-    results_table[idx, 9] = len(
-        plot_graph.get_unique_signatures(graph)
-    )  # num homotopy classes in homotopy augmented graph
-    results_table[idx, 10] = t  # time homotopy augmented graph
+    results_table[idx, 9] = t  # time homotopy augmented graph
+
+    # Fine resolution
+    print(
+        f"{CmdColors.WARNING}[GridGraph]{CmdColors.ENDC} Running graph with "
+        "resolution 0.25."
+    )
+    graph.set_grid_resolution(0.25, 0.25)
+    n_runs_fine = 1  # reduce number of runs for finer grid
+    t = (
+        timeit.timeit(
+            lambda graph=graph: graph.build_homotopy_augmented_graph(),
+            number=n_runs_fine,
+        )
+        / n_runs_fine
+    )
+    results_table[idx, 10] = len(graph.vertices_lift)  # nodes homotopy augmented graph
+    results_table[idx, 11] = t  # time homotopy augmented graph
 
     # Print intermediate results
     print(
@@ -183,11 +182,11 @@ for idx, case in enumerate(eval_cases):
     # Print results table and save to CSV file (updated after each eval case)
     with open("results/comparison_results.csv", "w", encoding="utf-8") as f:
         # Print header
-        print("#\tm\tl\t|T_2|\t|T'_2|\t|T'_0|\t|h|\tt\t|G'|\t|h|\tt")
+        print("env\tm\tl\t|T_2|\t|T'_2|\tt\t|G'|1\tt1\t|G'|.5\tt.5\t|G'|.25\tt.25")
         for i, row in enumerate(results_table):
             # index
-            print(f"#{int(row[0])}: ", end="")
-            f.write(f"{int(row[0])},")
+            print(f"{str(int(row[0]))}", end="")
+            f.write(f"{str(int(row[0]))},")
 
             # num obstacles (m)
             print(f"\t{int(row[1])}", end="")
@@ -198,36 +197,31 @@ for idx, case in enumerate(eval_cases):
             f.write(f"{row[2]:.1f},")
 
             # number of triangles in the base triangulation
-            print(f"\t {int(row[3])}", end="")
+            print(f"\t{int(row[3])}", end="")
             f.write(f"{int(row[3])},")
 
             # num triangles simplicial complex
             print(f"\t{int(row[4])}", end="")
             f.write(f"{int(row[4])},")
 
-            # num nodes simplicial complex
-            print(f"\t{int(row[5])}", end="")
-            f.write(f"{int(row[5])},")
+            # time simplicial complex
+            print(f"\t{row[5]:.2f} s", end="")
+            f.write(f"{row[5]:.2f},")
 
-            # num homotopy classes simplicial complex
+            # homotopy augmented graph with resolution 1
             print(f"\t{int(row[6])}", end="")
             f.write(f"{int(row[6])},")
-
-            # time simplicial complex
             print(f"\t{row[7]:.2f} s", end="")
-            f.write(f"{row[7]:.2f},")
+            f.write(f"{row[7]:.2f}\n")
 
-            # num nodes homotopy augmented graph
+            # homotopy augmented graph with resolution 0.5
             print(f"\t{int(row[8])}", end="")
             f.write(f"{int(row[8])},")
+            print(f"\t{row[9]:.2f} s", end="")
+            f.write(f"{row[9]:.2f}\n")
 
-            # num homotopy classes homotopy augmented graph
-            print(f"\t{int(row[9])}", end="")
-            f.write(f"{int(row[9])},")
-
-            # time homotopy augmented graph
-            print(f"\t{row[10]:.2f} s")
-            f.write(f"{row[10]:.2f}\n")
-
-    if SHOW_PLOTS is True:
-        plt.show()  # wait on user to close plot and continue
+            # homotopy augmented graph with resolution 0.1
+            print(f"\t{int(row[10])}", end="")
+            f.write(f"{int(row[10])},")
+            print(f"\t{row[11]:.2f} s")
+            f.write(f"{row[11]:.2f}\n")
