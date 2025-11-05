fbb70829498f7520447ad6009a8c8c452e283d9c
gbattocletti <g.battocletti@tudelft.nl>
2025-11-05 19:34:35 +0100

    Finalize generation of path planning example

--- scripts/path_planning.py
+++ scripts/path_planning.py
@@ -1,6 +1,9 @@
 """
-Generates a plot showing a path planning scenario in which all the homotopy classes
-between the robot and the goal are evaluated in parallel.
+Generates the following plots
+- an atlas for the environment defined in env_3.yaml where all the layers are shown.
+- a plot showing a path planning scenario in multiple homotopy classes between the robot
+and the goal are evaluated in parallel. The robot's paths and resulting tether
+configurations are shown.
 """
 
 import os
@@ -13,15 +16,16 @@ from shapely import Point
 
 from tethered_planning.env import env_2d
 from tethered_planning.env.triangulation import Triangulation
-from tethered_planning.utils import plot
+from tethered_planning.plan import graph_search
+from tethered_planning.utils import plot, plot_triangulation
 from tethered_planning.utils.colors import CmdColors
 from tethered_planning.utils.settings import Settings
 
 # Script settings
 SAVE_PNG = True
 SAVE_PGF = True
-SHOW_PLOTS = True
-filename = "env_2.yaml"  # set to None to open file dialog and select manually
+SHOW_PLOTS = False
+filename = "env_3.yaml"  # set to None to open file dialog and select manually
 
 # Check matplotlib version
 if version.parse(mpl.__version__) <= version.parse("3.7"):
@@ -38,75 +42,285 @@ abspath = os.path.abspath(__file__)
 dir_name = os.path.dirname(abspath)
 os.chdir(dir_name)
 
-# Find env name and base name for saving
-env_name = filename.replace("_", "-")
-env_name = env_name.replace(".yaml", "")
-base_name = "results/" + env_name
-
 # Create settings and env objects
 settings = Settings(create_sim_folder=False)
 settings.env_name = filename
 env = env_2d.Env2D(settings)
 
-# Generate triangulation
+# Generate triangulation and lift it
 triang = Triangulation(env)
 triang.triangulate()
+triang.max_lifted_triangles = 5000
+triang.set_max_dist(12.0)  # max tether length
+triang.lift_triangulation()
 
-# Perform path planning
-robot = []  # TODO
-tether = []  # TODO -- only for visualization and initial condition (lift)
-goal = np.array([1.0, 3.0])
-triang_goal = int(
-    triang.triang_tree.query(
-        Point(goal),
-        predicate="intersects",
-    )[0]
-)  # index of triangle containing the goal in the base triangulation
-triang_goal_lift = [
-    idx for idx, tri in enumerate(triang.vertices_dual_lift) if tri[0] == triang_goal
-]  # list of lifted triangles containing the goal
-
-length_list: list[float] = []
-geodesic_list: list[list[np.ndarray]] = []
-for idx, goal_lift in enumerate(triang_goal_lift):
-
-    # Find geodesics in different homotopy classes
-    length, geodesic = triang.geodesic_distance(
-        p1=goal,
-        s1=triang.vertices_dual_lift[goal_lift][1],
-        p2=env.anchor_point,
-        s2=[],
-        search_algorithm="dfs",
-    )
-    length_list.append(length)
-    geodesic_list.append(geodesic)
-
-# Plot and save the geodesic
-fig, _ = plot.plot_env(
+# Plot triangulation
+fig, _ = plot.plot_graph(
+    triang.vertices,
+    triang.edges,
     env,
-    show_tether=False,
-    show_robot=False,
-    show_anchor=True,
-    show_goal=False,
+    nodes_dual=triang.vertices_dual,
+    edges_dual=triang.edges_dual,
+    show_dual_graph=True,
+    label_nodes=False,
+    label_triangles=True,
+    show_generators=False,
+    show_generators_labels=False,
     show_legend=False,
-    show_generators=True,
-    points=[goal],  # goal point
-    curves=geodesic_list,  # geodesics
-    show_curves_labels=True,
-    show_generators_labels=True,
     show_obstacles_labels=True,
+    show_anchor=True,
+    show_robot=False,
+    show_tether=False,
     figsize=[4.5, 4.5],
 )
+if SHOW_PLOTS is True:
+    plt.show(block=False)
 
+# Initial conditions for path planning
+robot = [2, 2]
+tether = np.array(
+    [
+        [5.0, 4.5],
+        [5.0, 4.4],
+        [5.0, 4.3],
+        [5.0, 4.2],
+        [4.8, 4.1],
+        [4.5, 4.0],
+        [4.3, 3.8],
+        [4.0, 3.7],
+        [3.8, 3.6],
+        [3.5, 3.5],
+        [3.2, 3.2],
+        [3.0, 3.1],
+        [2.8, 3.0],
+        [2.6, 2.8],
+        [2.5, 2.6],
+        [2.4, 2.5],
+        [2.3, 2.4],
+        [2.2, 2.3],
+        [2.1, 2.2],
+        [2.0, 2.1],
+        [2.0, 2.0],
+    ]
+)
+goal = np.array([1.0, 9.0])
+env.robot_initial_pos = robot
+env.tether_state = tether
+
+# Generate 2D plot of the triangulation layers
+fig, ax = plot_triangulation.plot_2d(
+    triang,
+    env,
+    max_cols=5,
+    custom_sign_order=[
+        [],
+        [-5],
+        [-4],
+        [-1, 4],
+        [-1],
+        [-3, -2],
+        [-4, -2],
+        [-4, 1],
+        [6],
+    ],
+    add_env_subplot=True,
+    show_obstacles=False,
+    figsize=[18, 8],
+)
+
+# Manually update env subplot to show robot, goal, tether, and anchor
+plot.plot_env(
+    env,
+    show_generators=True,
+    show_generators_labels=True,
+    show_anchor=True,
+    show_tether=True,
+    tether=tether,
+    show_robot=True,
+    show_goal=False,
+    show_legend=False,
+    show_axes_labels=False,
+    target_ax=ax[0, 0],
+)
+ax[0, 0].plot(
+    goal[0],
+    goal[1],
+    color="green",
+    marker="o",
+    markersize=4,
+    zorder=10,
+)
+ax[0, 0].text(
+    0.5,
+    9.5,
+    "$x_\\mathrm{{{g}}}$",
+    fontsize=8,
+    zorder=10,
+)
+
+# Save the atlas figure
 if SAVE_PNG is True:
     fig.savefig(
-        base_name + "-paths.png",
+        "results/env3-atlas.png",
         dpi=300,
         format="png",
     )
 if SAVE_PGF is True:
     fig.savefig(
-        base_name + "-paths.pgf",
+        "results/env3-atlas.pgf",
+        format="pgf",
+    )
+
+# Perform path planning
+alpha_tether = [28, 16, 30, 13, 1, 18]
+
+# Find index of triangles containing the goal and robot in the base triangulation
+triang_goal = int(
+    triang.triang_tree.query(
+        Point(goal),
+        predicate="intersects",
+    )[0]
+)
+triang_robot = int(
+    triang.triang_tree.query(
+        Point(robot),
+        predicate="intersects",
+    )[0]
+)
+triang_anchor = int(
+    triang.triang_tree.query(
+        Point(env.anchor_point),
+        predicate="intersects",
+    )[0]
+)
+# Find list of triangles that are lifted copies of the one containing the goal and robot
+triang_goal_lift = [
+    idx for idx, tri in enumerate(triang.vertices_dual_lift) if tri[0] == triang_goal
+]
+triang_robot_lift = [
+    idx
+    for idx, tri in enumerate(triang.vertices_dual_lift)
+    if ((tri[0] == triang_robot) and (tri[1] == [-1]))
+][0]
+triang_anchor_lift = [
+    idx
+    for idx, tri in enumerate(triang.vertices_dual_lift)
+    if ((tri[0] == triang_anchor) and (tri[1] == []))
+][0]
+
+# Find geodesics in different homotopy classes
+path_length_list: list[float] = []
+path_list: list[list[np.ndarray]] = []
+tether_list: list[list[np.ndarray]] = []
+for idx, goal_lift in enumerate(triang_goal_lift):
+
+    # Compute geodesic path to travel
+    length, path = triang.geodesic_distance(
+        p1=goal,
+        s1=triang.vertices_dual_lift[goal_lift][1],
+        p2=robot,
+        s2=[-1],
+        search_algorithm="dfs",
+    )
+    path_length_list.append(length)
+    path_list.append(path)
+
+    # Compute representative alpha-path of the path (to compute resulting tether config)
+    alpha_tether_after_motion = graph_search.bfs(
+        triang.vertices_dual_lift,
+        triang.edges_dual_lift,
+        triang_anchor_lift,
+        goal_lift,
+    )
+    alpha_tether_after_motion = [
+        triang.vertices_dual_lift[idx][0] for idx in alpha_tether_after_motion
+    ]
+
+    # Compute geodesic tether configuration after motion
+    tether_after_motion = triang.homotopic_shortest_path(
+        alpha_tether_after_motion,
+        p_init=env.anchor_point,
+        p_end=goal,
+    )
+
+    # # Append to list
+    tether_list.append(tether_after_motion)
+
+# Initialize paths plot
+fig: plt.Figure
+axs: np.ndarray[plt.Axes]
+fig, axs = plt.subplots(
+    1,
+    5,
+    figsize=np.array([18, 4]) / 2.54,
+    constrained_layout=True,
+)
+
+# Add plots of geodesics
+for idx, path in enumerate(path_list):
+    if idx >= 5:
+        break  # limit to first 5 paths
+    plot.plot_env(
+        env,
+        show_tether=False,
+        show_robot=True,
+        show_anchor=True,
+        show_goal=False,
+        show_legend=False,
+        show_generators=False,
+        show_curves_labels=False,
+        show_generators_labels=False,
+        show_obstacles_labels=True,
+        show_axes_labels=False,
+        target_ax=axs[idx],
+    )
+    axs[idx].plot(
+        path[:, 0],
+        path[:, 1],
+        color="blue",
+        zorder=8,
+        linewidth=1,
+    )
+    axs[idx].plot(
+        tether_list[idx][:, 0],
+        tether_list[idx][:, 1],
+        color="black",
+        zorder=7,
+        linewidth=1.2,
+    )
+    axs[idx].plot(
+        goal[0],
+        goal[1],
+        color="green",
+        marker="o",
+        markersize=4,
+        zorder=10,
+    )
+    axs[idx].text(
+        0.5,
+        9.5,
+        "$x_\\mathrm{{{g}}}$",
+        fontsize=8,
+        zorder=10,
+    )
+    axs[idx].text(
+        4.7,
+        -1,
+        "$\\beta_{%d}$" % (idx + 1),
+        fontsize=8,
+        zorder=10,
+    )  # add text label below plot
+
+# Save the path planning figure
+if SAVE_PNG is True:
+    fig.savefig(
+        "results/env3-path-planning.png",
+        dpi=300,
+        format="png",
+    )
+if SAVE_PGF is True:
+    fig.savefig(
+        "results/env3-path-planning.pgf",
         format="pgf",
     )
 
